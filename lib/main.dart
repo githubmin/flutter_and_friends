cbaca993df806e4c29f7b005324f5042d32e263b
Daniel <deepblue.daniel@gmail.com>
2023-12-29 09:09:40 +0900

    chore: remove all bloc dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,13 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_and_friends/launchpad/launchpad.dart';
 import 'package:flutter_and_friends/theme/theme.dart';
 import 'package:flutter_and_friends/updater/updater.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: await getTemporaryDirectory(),
-  );
-  if (kDebugMode) await HydratedBloc.storage.clear();
   runApp(const ProviderScope(child: App()));
 }
 
